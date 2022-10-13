81fdbb304eed7b5c705daf52a2353fba25a34bee
Abreeza Saleem <abreeza.saleem@hotmail.com>
2022-10-13 16:57:36 -0400

    github pages added

--- src/apis/expenditures.js
+++ src/apis/expenditures.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import { parseJwt } from '../utils/index';
 
+const endpoint = process.env.REACT_APP_BUILD_ENV === "production"
+  ? "https://enigmatic-cliffs-45128.herokuapp.com"
+  : "http://localhost:8080";
+
 // block call if the token has been expired. Delete it from local storage as well
 export const fetchExpenditures = async () => {
   if (!localStorage.getItem('credential')) return;
   const token = localStorage.getItem('credential');
   const exp = parseJwt(token);
   // console.log(exp);
-  const response = await axios.get('http://localhost:8080/fetch-csv', {
+  const response = await axios.get(
+    `${endpoint}/fetch-csv`, {
     params: { email: 'ali.khilji94@gmail.com' },
     headers: {
       'Authorization': 'Bearer ' + localStorage.getItem('credential'),
@@ -16,7 +21,8 @@ export const fetchExpenditures = async () => {
   return response.data;
 }
 
-export const uploadExpenditureFile = async (formData) => await axios.post('http://localhost:8080/parse-csv', 
+export const uploadExpenditureFile = async (formData) => await axios.post(
+  `${endpoint}/parse-csv`, 
   formData, {
     headers: {
       'Content-Type': 'text/csv',
